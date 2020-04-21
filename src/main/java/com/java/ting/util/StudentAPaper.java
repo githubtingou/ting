aa31a9d12d73b4ee7cf27fa4c5b3f60aaedce036
lishuang <ShuangLiSH@wistronits.com>
2020-04-21 09:04:59 +0800

    feature测试提交

---
+++ src/main/java/com/java/ting/util/StudentAPaper.java
@@ -0,0 +1,19 @@
+package com.java.ting.util;
+
+public class StudentAPaper extends TestPaper {
+
+    @Override
+    String answer1() {
+        return "B";
+    }
+
+    @Override
+    String answer2() {
+        return "A";
+    }
+
+    @Override
+    String answer3() {
+        return "C";
+    }
+}
